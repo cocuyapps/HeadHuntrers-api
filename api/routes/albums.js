5e4fa93528876451d1033eea515c001eda526971
Diego Salas Noain <diegosalasnoain@Diegos-MacBook-Pro.local>
2019-04-19 12:55:13 -0500

    Added http operations for albums

--- api/routes/albums.js
+++ api/routes/albums.js
@@ -1,8 +1,94 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 
-router.get('/',(req,res,next)=>{
-    res.status(200).json({albums: 'album'});
-})
+const Album = require('../models/albums');
 
-module.exports=router;
+// /albums/?genre&offset&limit
+router.get('/', (req,res) => {
+    const offset = 0 || parseInt(req.query.offset);
+    const limit = 0 || parseInt(req.query.limit); // A limit value of 0 is equivalent to setting no limit.
+    const genre = req.query.genre;
+    if(genre){
+        const regex = new RegExp(genre,'i');
+        Album.find({genre: regex}).sort({likes: -1}).skip(offset).limit(limit).exec()
+            .then(docs => {
+                res.status(200).send(docs);
+            }).catch(err =>{
+                res.status(500).send({error: err});
+            });
+    }else{
+        Album.find().sort({likes: -1}).skip(offset).limit(limit).exec()
+        .then(docs => {
+            res.status(200).send(docs);
+        }).catch(err =>{
+            res.status(500).send({error: err});
+        });
+    }
+});
+
+router.get('/genres', (req,res) => {
+    Album.find().distinct('genre').exec()
+        .then(docs => {
+            const genres = [];
+            docs.forEach(doc => {
+                genres.push(doc);
+            });
+            res.status(200).send(genres);
+        })
+        .catch(err => {
+            res.status(500).send({error: err});
+        });
+});
+
+router.post('/', (req, res, next) => {
+    const {title,artist,url,image,thumbnail_image,songs,genre,likes} = req.body;
+    const album = new Album({
+        _id: new mongoose.Types.ObjectId(),
+        title,
+        artist,
+        url,
+        thumbnail_image,
+        image,
+        songs,
+        genre,
+        likes
+    });
+    album.save()
+    .then(result => {
+        res.status(201).send({message: 'createdAlbum:' + result});
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).send({error: err});
+    });
+});
+
+router.put('/:id', (req,res) => {
+    const {likes} = req.body;
+    const id = req.params.id;
+    console.log(req.body);
+    Album.findByIdAndUpdate(id,{$set: { likes }}, (err,doc) => {
+        if(err){
+            res.status(500).send({error: err});
+        }
+        res.status(200).send(`Updated succesful: ${doc.artist} - ${doc.title}`);
+    });
+});
+
+router.delete('/:id', (req, res, next) => {
+    Album.deleteOne({ _id: req.params.id })
+    .exec()
+    .then(result => {
+        res.status(200).json({
+            message: 'Album deleted'
+        });
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: err
+        });
+    });
+});
+
+module.exports = router;
--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express=require('express');
 const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const albumRoutes = require('./api/routes/albums');
 const userRoutes=require('./api/routes/user');
+
+mongoose.connect(
+    'mongodb+srv://node-headhunter:' + 
+    process.env.MONGO_ATLAS_PW + 
+    '@cluster0-8btrp.mongodb.net/test?retryWrites=true',
+    { 
+        useNewUrlParser: true
+    }
+)
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.use((req, res,next) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -20,14 +36,14 @@ app.use((req, res,next) => {
 app.use('/albums',albumRoutes);
 app.use('/users',userRoutes);
 
-app.use((req,res,next)=>{
+app.use((req,res,next) => {
     const error= new Error('Not Found');
     error.status=404;
     next(error);
 })
 
-app.use((error,req,res,next)=>{
-    res.status(error.status||500);
+app.use((error,req,res,next) => {
+    res.status(error.status || 500);
     res.json({
         error:{
             message: error.message
@@ -35,4 +51,4 @@ app.use((error,req,res,next)=>{
     });
 });
 
-module.exports=app;
+module.exports = app;
