0843cdb322118f9b912c427600d2247b46601095
diegosalas27 <dominicsc2hs@gmail.com>
2019-04-19 18:06:54 -0500

    added config

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const http = require('http');
 const app = require('./app')
 
-const port = process.env.port;
+const port = process.env.PORT || 3000;;
 const server= http.createServer(app);
 
+var config = {
+    appRoot: __dirname, // required config
+};
 
 server.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
