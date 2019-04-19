3d694155d8f1db53c8860d88b30e9ade1c0960bf
diegosalas27 <dominicsc2hs@gmail.com>
2019-04-19 18:10:57 -0500

    changed server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const http = require('http');
 const app = require('./app')
 
-const port = process.env.PORT || 3000;;
-const server= http.createServer(app);
+const PORT = process.env.PORT || 3000;
+const server = http.createServer(app);
 
 var config = {
     appRoot: __dirname, // required config
 };
 
-server.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
+server.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
 });
